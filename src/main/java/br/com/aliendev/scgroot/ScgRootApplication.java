896d9fed3443e319acc1bf9aeb723485ed9e18e0
Patrick Anjos <patrickpqdt87289@gmail.com>
2023-10-26 00:28:53 -0300

    feat(gateway): initial commit

---
+++ src/main/java/br/com/aliendev/scgroot/ScgRootApplication.java
@@ -0,0 +1,12 @@
+package br.com.aliendev.scgroot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScgRootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScgRootApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/aliendev/scgroot/ScgRootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aliendev.scgroot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScgRootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
